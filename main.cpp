4450b99a6e0ede55a23b6ca5436ffbc0d30fbbdd
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-03-12 12:20:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ float a;float b;float c;
 float Atriangolo= (a*b)/2; float Aquadrato= (a*a); float Arettangolo= (a*b); float Atrapezio= ((a+b)*c)/2;
  std::cin>>a>>b>>c;
  std::cout<<((a*b)/2)<<std::endl;
- std::cout<<((a*b))<<std::endl;
  std::cout<<((a*a))<<std::endl;
- std:: cout<<(((a+b)*c)/2)<<std::endl;
+ std::cout<<((a*b))<<std::endl;
+ std::cout<<(((a+b)*c)/2)<<std::endl;
   return 0;
 }
