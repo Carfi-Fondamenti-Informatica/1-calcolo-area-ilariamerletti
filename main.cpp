7e3b979a65d42acf7c36a7a6e06ad5dfc3809435
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-03-12 12:06:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-float a= 5, b= 3, c= 4;
- float triangolo= (a*b)/2;
+float triangolo= (a*b)/2;
 float rettangolo= (a*b);
 float quadrato= (a*a);
- float trapezio= (a+b)*c/2;
+float trapezio= (a+b)*c/2;
   cin >>a>>b>>c;
   cout<<((a*b)/2)<< endl;
   cout<<((a*b))<< endl;
   cout<<((a*a))<<endl;
-  cout<< ((a+b)*c/2);
+  cout<<((a+b)*c/2);
   return 0;
 }
-rat
